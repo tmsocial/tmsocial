519bb52f2cf57b33c02b28b1b2b1cb9b0b300416
Luca Versari <veluca93@gmail.com>
2019-03-13 02:38:54 +0100

    Implement EventManager and websocket client.

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -3,7 +3,7 @@ extern crate listenfd;
 use std::net::IpAddr;
 use std::path::PathBuf;
 
-use actix::{Addr, SyncArbiter};
+use actix::{Addr, Arbiter, SyncArbiter};
 use actix_web::http::header::HeaderValue;
 use actix_web::middleware::{ErrorHandlers, Response};
 use actix_web::{
@@ -15,7 +15,14 @@ use serde_derive::Serialize;
 
 mod db;
 mod endpoints;
+mod events;
 mod extractors;
+mod ws;
+
+pub struct State {
+    db: Addr<db::Executor>,
+    event_manager: Addr<events::EventManager>,
+}
 
 #[derive(Serialize)]
 pub struct ErrorResponse {
@@ -38,10 +45,6 @@ fn render_error<S>(
     Ok(Response::Done(resp))
 }
 
-pub struct State {
-    db: Addr<db::Executor>,
-}
-
 pub fn create_app(web_root: &PathBuf) -> App<State> {
     // bind all this error handlers
     let error_codes = vec![
@@ -56,8 +59,10 @@ pub fn create_app(web_root: &PathBuf) -> App<State> {
     let db_addr = SyncArbiter::start(3, || {
         db::Executor::new(crate::establish_connection())
     });
+    let event_manager = Arbiter::start(|_| events::EventManager::new());
     let mut app = App::with_state(State {
         db: db_addr.clone(),
+        event_manager: event_manager.clone(),
     })
     .middleware(middleware::Logger::default());
     for error_code in error_codes {
@@ -71,6 +76,7 @@ pub fn create_app(web_root: &PathBuf) -> App<State> {
     .resource("/api/user/{username}", |r| {
         r.method(http::Method::GET).with(endpoints::user::get_user)
     })
+    .resource("/api/events", |r| r.with(ws::events_handler))
     .resource("/api/contests", |r| {
         r.method(http::Method::GET)
             .with(endpoints::contest::get_contests)
---
+++ src/web/ws.rs
@@ -0,0 +1,115 @@
+use super::events::*;
+use super::State;
+use actix::prelude::*;
+use actix_web::{ws, Error, HttpRequest, HttpResponse};
+use log::{error, warn};
+use std::time::{Duration, Instant};
+
+/// How often heartbeat pings are sent
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+/// How long before lack of client response causes a timeout
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+
+struct UserEventSession {
+    id: usize,
+    user_id: i32,
+    hb: Instant,
+}
+
+impl Actor for UserEventSession {
+    type Context = ws::WebsocketContext<Self, State>;
+    fn started(&mut self, ctx: &mut Self::Context) {
+        // Heartbeat handling
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                warn!("Websocket Client heartbeat failed, disconnecting!");
+                ctx.state().event_manager.do_send(Disconnect {
+                    session_id: act.id,
+                    user_id: act.user_id,
+                });
+                ctx.stop();
+                return;
+            }
+            ctx.ping("");
+        });
+
+        // Register with event manager and store session id.
+        let addr = ctx.address();
+        ctx.state()
+            .event_manager
+            .send(Connect {
+                user_id: self.user_id,
+                rcp: addr.recipient(),
+            })
+            .into_actor(self)
+            .then(|res, act, ctx| {
+                match res {
+                    Ok(res) => act.id = res,
+                    Err(e) => {
+                        error!("Error registering client: {}", e);
+                        ctx.stop()
+                    }
+                }
+                fut::ok(())
+            })
+            .wait(ctx);
+    }
+
+    fn stopping(&mut self, ctx: &mut Self::Context) -> Running {
+        ctx.state().event_manager.do_send(Disconnect {
+            session_id: self.id,
+            user_id: self.user_id,
+        });
+        Running::Stop
+    }
+}
+
+impl Handler<super::events::Event> for UserEventSession {
+    type Result = ();
+
+    fn handle(&mut self, msg: super::events::Event, ctx: &mut Self::Context) {
+        let json = serde_json::to_string(&msg);
+        match json {
+            Ok(js) => ctx.text(js),
+            Err(e) => error!("Error during serialization: {}", e),
+        }
+    }
+}
+
+impl StreamHandler<ws::Message, ws::ProtocolError> for UserEventSession {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        println!("WEBSOCKET MESSAGE: {:?}", msg);
+        match msg {
+            ws::Message::Ping(msg) => {
+                self.hb = Instant::now();
+                ctx.pong(&msg);
+            }
+            ws::Message::Pong(_) => {
+                self.hb = Instant::now();
+            }
+            ws::Message::Text(text) => {
+                warn!("Unexpected text from client: {}", text)
+            }
+            ws::Message::Binary(bin) => {
+                warn!("Unexpected binary from client: {:?}", bin)
+            }
+            ws::Message::Close(_) => {
+                ctx.stop();
+            }
+        }
+    }
+}
+
+pub fn events_handler(
+    req: HttpRequest<State>,
+    user: crate::models::User,
+) -> Result<HttpResponse, Error> {
+    ws::start(
+        &req,
+        UserEventSession {
+            id: 0,
+            user_id: user.id,
+            hb: Instant::now(),
+        },
+    )
+}
