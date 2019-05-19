47ea7aef5bfb8b92959c179393a260ffca5be579
alerighi <alessandro.righi@outlook.it>
2019-05-19 15:08:53 +0200

    Implement new adapter

--- task_maker_wrapper/adapter.py
+++ task_maker_wrapper/adapter.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+import os
+import sys
 import json
+import subprocess
+
+
+TASK_MAKER = "task-maker"
 
 
 def generate_value_event(key, value):
@@ -15,12 +21,12 @@ def process_testcase_outcome(event):
     yield from generate_value_event(path + "message", data["message"])
 
 
-def process_result(event):
-    subtask_results = event["testing"]["solution.cpp"]["subtask_results"]
+def process_result(event, source):
+    subtask_results = event["testing"][source]["subtask_results"]
     for subtask, result in subtask_results.items():
         yield from generate_value_event(f"subtask.{subtask}.result", result)
 
-    testcase_results = event["testing"]["solution.cpp"]["testcase_results"]
+    testcase_results = event["testing"][source]["testcase_results"]
     for subtask, testcases in testcase_results.items():
         for testcase, result in testcases.items():
             path =  f"subtask.{subtask}.testcase.{testcase}."
@@ -31,21 +37,45 @@ def process_result(event):
             yield from generate_value_event(path + "return_code", result["result"][0]["return_code"])
     
 
-def process_event(event):
+def process_event(event, source):
     action = event["action"]
     if action == "testcase-outcome":
         yield from process_testcase_outcome(event)
     if action == "result":
-        yield from process_result(event)
+        yield from process_result(event, source)
+
+
+def start_task_maker(*, task_dir, submission_dir, evaluation_dir):
+    files_dir = os.path.join(submission_dir, "files")
+    submitted_files = list(os.path.abspath(os.path.join(files_dir, f)) for f in os.listdir(files_dir))
+
+    with open(os.path.join(evaluation_dir, "events.jsonl"), "w") as out:
+        with subprocess.Popen([
+            TASK_MAKER,
+            "--ui", "json",
+            "--task-dir", task_dir,
+            *submitted_files
+        ], stdout=subprocess.PIPE) as p:
+
+            for line in p.stdout:
+                for e in process_event(json.loads(line), os.path.split(submitted_files[0])[1]):
+                    print(json.dumps(e), file=out)
+
+        print(json.dumps({
+            "type": "end",
+            "return_code": p.returncode,
+        }), file=out)
 
 
 def main():
-    while True:
-        try:
-            for e in process_event(json.loads(input())):
-                print(json.dumps(e))
-        except EOFError:
-            exit(0)
+    if len(sys.argv) < 4:
+        print(f"Usage: {sys.argv[0]} <task_dir> <submission_dir> <evaluation_dir>", file=sys.stderr)
+        exit(1)
+    start_task_maker(
+        task_dir=sys.argv[1],
+        submission_dir=sys.argv[2],
+        evaluation_dir=sys.argv[3],
+    )
 
 
 if __name__ == "__main__":
