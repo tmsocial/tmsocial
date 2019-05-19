8bcdacbdce6ca1eb30bfcee67828a40109c33d2f
Massimo Cairo <cairomassimo@gmail.com>
2019-05-19 19:55:58 +0200

    Web UI: use WS transport to support subscriptions

--- web_ui/src/index.tsx
+++ web_ui/src/index.tsx
@@ -1,15 +1,17 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
 import gql from "graphql-tag";
 import * as React from "react";
-import { ApolloProvider, Query, QueryResult } from "react-apollo";
+import { ApolloProvider, Query, QueryResult, Subscription, SubscriptionResult } from "react-apollo";
 import * as ReactDOM from "react-dom";
 import { Contest } from "./__generated__/Contest";
+import { EvaluationEvents } from "./__generated__/EvaluationEvents";
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
+  link: new WebSocketLink({
+    uri: "ws://localhost:4000/graphql",
   }),
   cache: new InMemoryCache(),
 });
@@ -41,6 +43,17 @@ const App = () => (
               : null
       )}
     </Query>
+    <Subscription subscription={gql`
+      subscription EvaluationEvents {
+        evaluation_events(evaluation_id: "site1/default/user1/easy1/2019-05-19T17:35:53.174Z/2019-05-19T17:35:53.175Z") {
+          json
+        }
+      }
+    `}>
+      {({ loading, error, data }: SubscriptionResult<EvaluationEvents>) => (
+        <pre>{data && data.evaluation_events.json}</pre>
+      )}
+    </Subscription>
   </ApolloProvider>
 )
 
