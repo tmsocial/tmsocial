ecd6303d157bd0d82848f67373f430c1e01cd68d
alerighi <alessandro.righi@outlook.it>
2019-05-19 11:28:32 +0200

    Implement some resolvers

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,3 +1,6 @@
+import { readFileSync, readdirSync } from "fs";
+import { join } from "path";
+
 async function* makeDummyStream() {
   yield {
     evaluation_events: {
@@ -12,8 +15,54 @@ async function* makeDummyStream() {
   };
 };
 
+const CONFIG_DIRECTORY = '/Users/ale/git/tmsocial/test_site/config';
+const DATA_DIRECTORY = '';
+
+async function loadConfig(path: string): Promise<any> {
+  let file;
+  try {
+    file = readFileSync(join(CONFIG_DIRECTORY, path, 'data.json'), 'utf8');
+  } catch(e) {
+    console.log(e);
+    return null;
+  }
+
+  const content = JSON.parse(file);
+  return {
+    path, 
+    ...content,
+  }
+}
+
 export const resolvers = {
   Query: {
+    site(obj: any, { id }: {id: string}) {
+      return {
+        path: `/${id}/`,
+      };
+    },
+   },
+  Site: {
+    async default_contest({path}: {path: string}) {
+      return await loadConfig(join(path, 'contests/default/'));
+    },
+    async user({path}: {path: string}, {id: user_id}: {id: string}) {
+      return await loadConfig(join(path, 'users/', user_id));
+    },
+  },
+  Contest: {
+    async tasks({path}: {path: string}) {
+      const dir = readdirSync(join(CONFIG_DIRECTORY, path, 'tasks/'));
+      return dir.map(e => ({path: join(path, 'tasks', e), id: e}));
+    },
+    async task({path}: {path: string}, id: string) {
+      return {path: join(path, 'tasks', id), id}
+    },
+  },
+  ContestTask: {
+    id({id}: {id: string}) {
+      return id;
+    }
   },
   Mutation: {
   },
