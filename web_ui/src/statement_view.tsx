b9aa6ea3c1a083f54a63786e257f50d06d4f55fa
Massimo Cairo <cairomassimo@gmail.com>
2019-05-21 00:06:50 +0200

    Some CSS :)

--- web_ui/src/statement_view.tsx
+++ web_ui/src/statement_view.tsx
@@ -4,7 +4,7 @@ import { localize } from './l10n';
 
 export const StatementView = ({ statement }: { statement: Statement }) => (
     <React.Fragment>
-        {statement.html && <iframe srcDoc={localize(statement.html)} sandbox="allow-scripts" />}
         {statement.pdf_base64 && <a download href={`data:application/pdf;base64,${localize(statement.pdf_base64)}`}>Download PDF</a>}
+        {statement.html && <iframe className="task_statement_html" srcDoc={localize(statement.html)} sandbox="allow-scripts" />}
     </React.Fragment>
 )
