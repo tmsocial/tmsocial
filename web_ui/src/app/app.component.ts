e8964bd8ade86fb19d9aec2613e61fed7a77dc14
Massimo Cairo <cairomassimo@gmail.com>
2019-05-23 12:24:10 +0200

    Angular UI: implement first GrraphQL query

--- web_ui/src/app/app.component.ts
+++ web_ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-web-ui';
+
+
+  constructor(private apollo: Apollo) { }
+
+  queryRef = this.apollo.watchQuery({
+    query: gql`
+      query AppQuery {
+        site(id: "site1") {
+          id
+        }
+      }
+    `,
+  });
+
+  dataJson: string;
+
+  subscription = this.queryRef.valueChanges.subscribe(({ data, loading }) => {
+    this.dataJson = JSON.stringify(data);
+  });
 }
