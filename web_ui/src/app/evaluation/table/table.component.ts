8c1361be5986cc4ec85c6d18981d670db9eed017
Massimo Cairo <cairomassimo@gmail.com>
2019-05-27 22:12:56 +0200

    Implement coloring evaluation rows

--- web_ui/src/app/evaluation/table/table.component.ts
+++ web_ui/src/app/evaluation/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { EvaluationReducer } from 'src/evaluation-process';
-import { Column, Table } from 'src/table';
+import { evaluateExpression } from 'src/app/evaluate.pipe';
+import { Status } from 'src/evaluation';
+import { EvaluationReducer, EvaluationState } from 'src/evaluation-process';
+import { Column, Row, Table, ValueCell } from 'src/table';
 
 @Component({
   selector: 'app-table',
@@ -20,4 +22,17 @@ export class TableComponent {
   columnTrackBy(column: Column, index: number) {
     return index;
   }
+
+  getStatusClass(status: Status | null) {
+    return status !== null ? status.status : 'status_unknown';
+  }
+
+  getRowClasses(table: Table, row: Row, state: EvaluationState) {
+    return table.columns
+      .map((column, i) => ({ column, cell: row.cells[i] }))
+      .filter(({ column }) => column.type === 'row_status')
+      .map(({ cell }) => cell as ValueCell<Status>)
+      .map(cell => `row_status ${this.getStatusClass(evaluateExpression(cell.value, state))}`);
+  }
+
 }
