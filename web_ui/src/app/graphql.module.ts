b732cb69c3deb14ec18ae220cff10aab73afbc6b
Massimo Cairo <cairomassimo@gmail.com>
2019-05-27 22:16:50 +0200

    Connect to correct graphql endpoint

--- web_ui/src/app/graphql.module.ts
+++ web_ui/src/app/graphql.module.ts
@@ -3,10 +3,22 @@ import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+function graphqlUrl() {
+  // https://stackoverflow.com/questions/10406930/how-to-construct-a-websocket-uri-relative-to-the-page-uri
+  let graphqlProtocol;
+  if (window.location.protocol === 'https:') {
+    graphqlProtocol = 'wss:';
+  } else {
+    graphqlProtocol = 'ws:';
+  }
+  return `${graphqlProtocol}//${window.location.host}${window.location.pathname}graphql`;
+}
+
 export function createApollo() {
+
   return {
     link: new WebSocketLink({
-      uri: 'ws://localhost:4000/graphql'
+      uri: window.location.toString() === 'http://localhost:4200/' ? 'ws://localhost:4000/graphql' : graphqlUrl()
     }),
     cache: new InMemoryCache(),
   };
